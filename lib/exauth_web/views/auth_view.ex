50aa497088a582c7c490d4870114d825c0b60d45
ryuzaki <ryuzaki>
2022-04-28 20:38:28 +0530

    auth views

--- lib/exauth_web/views/auth_view.ex
+++ lib/exauth_web/views/auth_view.ex
@@ -1,5 +1,9 @@
 defmodule ExauthWeb.AuthView do
   use ExauthWeb, :view
 
-  def render("ack.json", %{success: success, message: message}), do: %{success: success, message: message}
+  def render("ack.json", %{success: success, message: message}),
+    do: %{success: success, message: message}
+
+  def render("errors.json", %{errors: errors}), do: %{success: false, errors: errors}
+  def render("error.json", %{error: error}), do: %{success: false, error: error}
 end
